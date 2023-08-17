65de42e406ce30d91081e4e72f5e2bdb4b15fd9f
CarolinaGuzman89 <cguzmanvazq@gmail.com>
2023-08-17 11:45:12 -0600

    responsive design improvements

--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -4,34 +4,26 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import ResultsContainer from './ResultsContainer';
 
 
-
+const ipAddresTracker = {
+    ipAddress: "8.8.8.8",
+    location: "California",
+    city: "Mountain View",
+    cp: "94035",
+    timezone: "-07:00",
+    isp: "Google LLC",
+}
 
 
 export default function Principal() {
-    const [searchIp, setSearchIp] = useState({
-        ip:"",
-    });
 
-    const [data, setData] = useState({
-        ipAddress: "8.8.8.8",
-        location: "California",
-        city: "Mountain View",
-        cp: "94035",
-        timezone: "-07:00",
-        isp: "Google LLC",
-    })
+    const [ip, setIp] = useState("")
+    const [ipData, setIpData] = useState(ipAddresTracker)
     
     const [position, setPosition] = useState([37.38605, -122.08385]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        
-    }
-    
+
     const handleChange = event => {
-        setSearchIp({
-            ip: event.target.value
-        })
+        setIp(event.target.value)
     }
 
     function FlyMapTo() {
@@ -45,13 +37,15 @@ export default function Principal() {
         return null
     }
     
-    const handleClick = (() =>{
-        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qJOatF00NclCcOgJIG3O2IM9pn4g8&ipAddress=${searchIp.ip}`)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qJOatF00NclCcOgJIG3O2IM9pn4g8&ipAddress=${ip}`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            setData(
-                
+            setIpData(
                     {
                         ipAddress: data.ip,
                         location: data.location.region,
@@ -61,51 +55,44 @@ export default function Principal() {
                         isp: data.as.name,
                     }
             )
-            
             setPosition([data.location.lat, data.location.lng])
-
         })
-
-        setSearchIp({
-            ip: "",
-        })
-
-        
-        
-    })
+        setIp("")
+    }
 
     return (
-        <div className='grid grid-cols-1'>
-            <div id='background' className='grid grid-rows-3 w-full pt-10 '> 
-                <h1>IP Address Tracker</h1>
-                <div className="w-full m-10 " >
-                    <div className='w-1/3 h-12 inline-flex rounded-lg bg-white mb-10'>
-                        <form onSubmit={handleSubmit} className='bg-white w-full flex rounded-lg '> 
+        <div className='flex justify-center flex-col '>
+            <div id='background' className='pt-10'> 
+                <h1 className='mb-5'>IP Address Tracker</h1>
+                <div className='flex justify-center'>
+                    <div className='h-12 mt-5 w-80 relative shadow-sm'>
+                        <form onSubmit={handleSubmit} className=""> 
                             <input 
-                                value={searchIp.ip}
+                                value={ip}
                                 onChange={handleChange}
                                 type="text"
                                 name="search"
                                 id="search"
                                 placeholder='Search for any address or domain'
-                                className='bg-white text-sm  pl-5 w-5/6 rounded-lg cursor-pointer md:text-xs'
+                                className="bg-white h-12 p-5 w-full rounded-lg border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  "
                                 required
                             />
-                            <button type="submit" onClick={handleClick} className='w-1/6 h-12 self-center bg-black rounded-r-lg cursor-pointer hover:bg-zinc-800'><ChevronRightIcon className=" text-slate-200 w-8" aria-hidden="true" /></button>
-                        </form>
-                        
-                    </div>
+                            <button type="submit"  className="bg-black rounded-r-lg border-black absolute inset-y-0 right-0 flex items-center pr-3">
+                                <ChevronRightIcon className=" text-slate-200 h-5 w-5" aria-hidden="true" />
+                            </button>
+                        </form>  
+                    </div> 
                 </div>
-                <ResultsContainer 
-                    ip={data.ipAddress} 
-                    region={data.location} 
-                    city={data.city} 
-                    cp={data.cp} 
-                    timezone={data.timezone} 
-                    isp={data.isp}
-                    />
             </div>
-            <div className='w-full  bg-white overflow-auto touch-pan-y' >
+            <ResultsContainer 
+                    ip={ipData.ipAddress} 
+                    region={ipData.location} 
+                    city={ipData.city} 
+                    cp={ipData.cp} 
+                    timezone={ipData.timezone} 
+                    isp={ipData.isp}
+            />
+            <div className='w-full z-0  bg-white  touch-pan-y ' >
                 <MapContainer
                 center={position}
                 zoom={13}
@@ -119,8 +106,8 @@ export default function Principal() {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     zIndex={10}
                 />
-                <Marker position={position}>
-                    <Popup>
+                <Marker position={position} >
+                    <Popup icon={"../assets/marker-icon-2x.png"}>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
                 </Marker>
--- src/Components/ResultsContainer.jsx
+++ src/Components/ResultsContainer.jsx
@@ -5,22 +5,22 @@ import React from 'react'
 export default function ResultsContainer({ip, region, city, cp, timezone, isp}) {
 
     return (
-        <div className='bg-white text-xl mt-10 rounded-lg grid  grid-cols-4 w-5/6 h-32 p-5 text-left divide-x mx-20 md:text-sm '>
-            <div className='pl-5'>
-                <h2 >IP ADDRESS</h2>
-                <p id='ipAddress' key={ip}>{ip}</p>
+        <div className='bg-white text-md m-12  text-center gap-1  absolute top-40 z-10 rounded-lg grid  grid-cols-1 grid-rows-4 md:grid-cols-4 w-5/6 md:h-44 p-5 md:text-left md:mx-20 md:text-lg '>
+            <div className='px-5 '>
+                <h2 className='text-gray-400 text-sm pb-2'>IP ADDRESS</h2>
+                <p className='font-semibold text-lg' id='ipAddress' key={ip}>{ip}</p>
             </div>
-            <div className='pl-5'>
-                <h2>LOCATION</h2>
-                <p id='location' key={region}>{region}, {city} {cp}</p>
+            <div className='px-5 '>
+                <h2 className='text-gray-400 text-sm pb-2'>LOCATION</h2>
+                <p className='font-semibold text-lg' id='location' key={region}>{region}, {city} {cp}</p>
             </div>
-            <div className='pl-5'>
-                <h2>TIMEZONE</h2>
-                <p id='timezone' key={timezone}>{timezone}</p>
+            <div className='px-5 '>
+                <h2 className='text-gray-400 text-sm pb-2'>TIMEZONE</h2>
+                <p className='font-semibold text-lg' id='timezone' key={timezone}>{timezone}</p>
             </div>
-            <div className='pl-5'>
-                <h2>ISP</h2>
-                <p id='isp' key={isp}>{isp}</p>
+            <div className='px-5 pb-2'>
+                <h2 className='text-gray-400 text-sm pb-2'>ISP</h2>
+                <p className='font-semibold text-lg' id='isp' key={isp}>{isp}</p>
             </div>        
         </div>
     )
